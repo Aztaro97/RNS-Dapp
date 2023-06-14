08478fb030bff300e72f9fe7d883af09cedac8fc
aztaro97 <aztaro97@gmail.com>
2023-06-15 02:18:11 +0400

    feat:create cart page

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -7,6 +7,7 @@ import { MainNav } from "@/components/main-nav"
 import { ThemeToggle } from "@/components/theme-toggle"
 
 import UserAvatar from "./userAvatar"
+import NavShoppingCart from "./navCart"
 
 export function SiteHeader() {
   return (
@@ -14,9 +15,10 @@ export function SiteHeader() {
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <MainNav items={siteConfig.mainNav} />
         <div className="flex flex-1 items-center justify-end space-x-4">
-          <nav className="flex items-center space-x-1">
-            <UserAvatar />
+          <nav className="flex items-center space-x-2">
             <ThemeToggle />
+            <UserAvatar />
+			<NavShoppingCart />
           </nav>
         </div>
       </div>
--- components/userAvatar/index.tsx
+++ components/userAvatar/index.tsx
@@ -4,21 +4,51 @@ import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar"
 import {
   DropdownMenu,
   DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
   DropdownMenuLabel,
+  DropdownMenuShortcut,
   DropdownMenuTrigger,
 } from "../ui/dropdown-menu"
+import { Files, LogOut, User, Wallet, List } from "lucide-react"
 
 const UserAvatar = () => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Avatar className="cursor-pointer">
+        <Avatar className="cursor-pointer h-8 w-8">
           <AvatarImage src="https://i.pravatar.cc" alt="SVG" />
           <AvatarFallback>Taro</AvatarFallback>
         </Avatar>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-48">
-        <DropdownMenuLabel>Hello word</DropdownMenuLabel>
+		<DropdownMenuGroup>
+          <DropdownMenuItem>
+            <Wallet className="mr-2 h-4 w-4" />
+            <span>0x9a88140c7c</span>
+            <DropdownMenuShortcut>
+				<Files className="h-4 w-4" />
+			</DropdownMenuShortcut>
+          </DropdownMenuItem>
+		</DropdownMenuGroup>
+		<DropdownMenuGroup>
+          <DropdownMenuItem>
+            <User className="mr-2 h-4 w-4" />
+            <span>Profile</span>
+          </DropdownMenuItem>
+		</DropdownMenuGroup>
+		<DropdownMenuGroup>
+          <DropdownMenuItem>
+            <List className="mr-2 h-4 w-4" />
+            <span>My Domains</span>
+          </DropdownMenuItem>
+		</DropdownMenuGroup>
+		<DropdownMenuGroup>
+          <DropdownMenuItem>
+            <LogOut className="mr-2 h-4 w-4" />
+            <span>Sign Out</span>
+          </DropdownMenuItem>
+		</DropdownMenuGroup>
       </DropdownMenuContent>
     </DropdownMenu>
   )
